1961df70026e24883515e7f0aeb1460ea3bab667
nyan42 <victor-legros2@hotmail.fr>
2020-11-04 10:12:25 +0100

    mise en page

--- post.php
+++ post.php
@@ -4,27 +4,27 @@
 	<p>Commentaire<br /><textarea name="commentaire" id="commentaire"></textarea></p>
 	<p>Lien de la photo<br /><input type="text" name="photo" id="photo" /></p>
 	<p>Adresse e-mail<br /><input type="text" name="email" id="email" class="hidden" />
-	<div class="input-group">
-		<button type="submit" class="btn" name="reg_posts">Poster</button>
-	</div>
-	<div class="clear"></div>
-	<div class="clear"></div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="reg_posts">Poster</button>
+		</div>
+		<div class="clear"></div>
+		<div class="clear"></div>
 </form>
 
 <?php
-require ("bdd/dbSet.php");
+require("bdd/dbSet.php");
 
 
 
 if (isset($_POST['reg_posts'])) {
-    // receive all input values from the form
-    $commentaire = mysqli_real_escape_string($db, $_POST['commentaire']);
-    $photo = mysqli_real_escape_string($db, $_POST['photo']);
-    $email = mysqli_real_escape_string($db, $_POST['email']);
+	// receive all input values from the form
+	$commentaire = mysqli_real_escape_string($db, $_POST['commentaire']);
+	$photo = mysqli_real_escape_string($db, $_POST['photo']);
+	$email = mysqli_real_escape_string($db, $_POST['email']);
 
-    $query = "INSERT INTO posts (photo, commentaire, email) 
+	$query = "INSERT INTO posts (photo, commentaire, email) 
            VALUES('$photo', '$commentaire', '$email')";
-    mysqli_query($db, $query);
+	mysqli_query($db, $query);
 }
 
 ?>
