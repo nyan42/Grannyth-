663086e52649062dbfc3869c05def1bde9fea281
nyan42 <victor-legros2@hotmail.fr>
2020-11-04 16:29:54 +0100

    publication

--- publication.php
+++ publication.php
@@ -1,36 +1,196 @@
-<?php
+<!DOCTYPE HTML>
+<!--
+	SmartCityzen - copyright
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
 
-require("bdd/dbSet.php");
+<head>
+    <title>Publications</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
 
-session_start();
+<body class="is-preload">
 
-if (isset($_SESSION['username'])){
-    $username = $_SESSION['username'];
-}
+    <!-- Header -->
+    <header id="header">
+        <a class="logo" href="index.php">SmartCityzen</a>
+        <nav>
+            <a href="#menu">Menu</a>
+        </nav>
+    </header>
 
-if (isset($_POST['reg_posts'])) {
-    // receive all input values from the form
-    $commentaire = mysqli_real_escape_string($db, $_POST['commentaire']);
-    $photo = mysqli_real_escape_string($db, $_POST['photo']);
+    <!-- Nav -->
+    <nav id="menu">
+        <ul class="links">
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </nav>
 
-    $query = "INSERT INTO posts (photo, commentaire, email) 
+    <!-- Heading -->
+    <div id="heading">
+        <h1>Publications</h1>
+    </div>
+
+    <!-- Main -->
+    <section id="main" class="wrapper">
+        
+        <div class="inner">
+        <a href="./index.php"> Retour <a>
+
+            <div class="content">
+                <?php
+
+                require("bdd/dbSet.php");
+
+                session_start();
+
+                if (isset($_SESSION['username'])) {
+                    $username = $_SESSION['username'];
+                }
+
+                if (isset($_POST['reg_posts'])) {
+                    // receive all input values from the form
+                    $commentaire = mysqli_real_escape_string($db, $_POST['commentaire']);
+                    $photo = mysqli_real_escape_string($db, $_POST['photo']);
+
+                    $query = "INSERT INTO posts (photo, commentaire, email) 
         VALUES ('$photo', '$commentaire', '$username')";
-    mysqli_query($db, $query);
-}
-/*
+                    mysqli_query($db, $query);
+                }
+                /*
 $sql = "SELECT city FROM users WHERE username = '$username'";
 $requete= $db->query($sql);
 while ($resultat = $requete->fetch_assoc()){
     $city = $resultat['city'];
 }
 echo $city;*/
-$sql = "SELECT commentaire, photo FROM posts, users WHERE posts.email = users.username and users.username = '$username'";
-$requete= $db->query($sql);
-while ($resultat = $requete->fetch_assoc()){
-    $commentaire = $resultat['commentaire'];
-    $photo = $resultat['photo'];
-    echo '<p>'.$commentaire.'</p>';
-    echo '<img src=images/'.$photo.'>';
-}
-?>
+                $sql = "SELECT commentaire, photo FROM posts, users WHERE posts.email = users.username and users.username = '$username'";
+                $requete = $db->query($sql);
+                while ($resultat = $requete->fetch_assoc()) {
+                    $commentaire = $resultat['commentaire'];
+                    $photo = $resultat['photo'];
+                    echo '<p>' . $commentaire . '</p>';
+                    echo '<img src=images/' . $photo . '>';
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+  
+    
+    <div class="card-columns">
+  <div class="card">
+    <img src="images/pic01.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title that wraps to a new line</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+  </div>
+  <div class="card p-3">
+    <blockquote class="blockquote mb-0 card-body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card bg-primary text-white text-center p-3">
+    <blockquote class="blockquote mb-0">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+      <footer class="blockquote-footer text-white">
+        <small>
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card text-center">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has a regular title and short paragraphy of text below it.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+  </div>
+  <div class="card p-3 text-right">
+    <blockquote class="blockquote mb-0">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
 
+    <!-- Footer -->
+    <footer id="footer">
+        <div class="inner">
+            <div class="content">
+                <section>
+                    <h3>Accumsan montes viverra</h3>
+                    <p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
+                </section>
+                <section>
+                    <h4>Sem turpis amet semper</h4>
+                    <ul class="alt">
+                        <li><a href="#">Dolor pulvinar sed etiam.</a></li>
+                        <li><a href="#">Etiam vel lorem sed amet.</a></li>
+                        <li><a href="#">Felis enim feugiat viverra.</a></li>
+                        <li><a href="#">Dolor pulvinar magna etiam.</a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h4>Magna sed ipsum</h4>
+                    <ul class="plain">
+                        <li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+                        <li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+                        <li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+                        <li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+                    </ul>
+                </section>
+            </div>
+            <div class="copyright">
+                &copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scripts -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
