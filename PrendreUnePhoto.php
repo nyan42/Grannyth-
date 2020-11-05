54276bcd83a1a0926043b10f67015e824f6170ec
LydieV <49896819+LydieV@users.noreply.github.com>
2020-11-05 15:32:13 +0100

    modifs css menu

--- PrendreUnePhoto.php
+++ PrendreUnePhoto.php
@@ -18,45 +18,27 @@
 <body>
 <div id="root">
 
-	<!-- Header -->
-	<header id="header">
-		<a class="logo" href="index.php">SmartCityzen</a>
-		<nav>
-			<a href="#menu">Menu</a>
-		</nav>
-	</header>
+    <!-- Header -->
+    <header id="header">
+        <a class="logo" href="index.php">
+            <img src="images/SmartCitizen_blanc.png" class="logo_img" />
+        </a>
+        <nav>
+            <a href="#menu"></a>
+        </nav>
+    </header>
 
-	<!-- Nav -->
-	<nav id="menu">
-		<ul class="links">
-			<li><a href="index.php">Home</a></li>
-		</ul>
-	</nav>
-
-	<!-- Heading -->
-	<!--<div id="heading">
-		<h1>Prendre une photo</h1>
-	</div>-->
-
-	<!-- Main -->
-	<!--<section id="main" class="wrapper">
-		<div class="inner">
-			<div class="content">
-				<div class="ImageTrois">
-					<div class="textfacile">
-						<input type="image" id="avatar" name="avatar" accept="image/png, image/jpeg" src="images/InsertPhoto.png">
-					</div>
-				</div>
-				<div class="ImageTrois">
-					<div class="textfacile">
-						<a href="./index.php"> Retour </div> </a>
-					<div class="textfacile">
-						<a href="post.php"> Publier la photo </a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>-->
+    <!-- Nav -->
+    <nav id="menu">
+        <ul class="links">
+            <li><a href="PrendreUnePhoto.php">Prendre une photo </a></li>
+            <li><a href="post.php">Publier une proposition </a></li>
+            <li><a href="publicationGeneral.php">Voir les propositions </a></li>
+            <li><a href="registration/index.php"> Mon profil </a></li>
+            <li><a href="#"> SmartCitizen, c'est quoi ? </a></li>
+            <li><a href="index.php?logout='1'">Déconnexion </a></li>
+        </ul>
+    </nav>
 
 	<section class="prendre_photo" id="main">
 		<a href="./index.php">
@@ -69,40 +51,6 @@
 			<a href="post.php" class="button_CTA"> Publier la photo </a>
 		</div>
 	</section>
-
-
-	<!-- Footer -->
-	<!--<footer id="footer">
-		<div class="inner">
-			<div class="content">
-				<section>
-					<h3>Accumsan montes viverra</h3>
-					<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
-				</section>
-				<section>
-					<h4>Sem turpis amet semper</h4>
-					<ul class="alt">
-						<li><a href="#">Dolor pulvinar sed etiam.</a></li>
-						<li><a href="#">Etiam vel lorem sed amet.</a></li>
-						<li><a href="#">Felis enim feugiat viverra.</a></li>
-						<li><a href="#">Dolor pulvinar magna etiam.</a></li>
-					</ul>
-				</section>
-				<section>
-					<h4>Magna sed ipsum</h4>
-					<ul class="plain">
-						<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
-						<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
-						<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
-						<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
-					</ul>
-				</section>
-			</div>
-			<div class="copyright">
-				&copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
-			</div>
-		</div>
-	</footer>-->
 
 	<!-- Scripts -->
 	<script src="assets/js/jquery.min.js"></script>
--- registration/register.php
+++ registration/register.php
@@ -25,89 +25,11 @@
 		<div>
 			<!-- Header -->
 			<header id="header">
-				<a class="logo" href="../index.php">SmartCityzen</a>
-				<nav>
-					<a href="#menu">Menu</a>
-				</nav>
+                <a class="logo_firstpages" href="#">
+                    <img src="../images/SmartCitizen_blanc.png" class="logo_img_firstpages" />
+                </a>
 			</header>
 
-			<!-- Nav -->
-			<nav id="menu">
-				<ul class="links">
-					<li><a href="../index.php">Home</a></li>
-				</ul>
-			</nav>
-
-			<!-- Heading -->
-			<!--<div id="heading">
-		<h1>Inscription</h1>
-	</div>-->
-
-			<!-- Main -->
-			<!--<section id="main" class="wrapper">
-		<div class="inner">
-
-
-			<div class="container">
-				<div class="row align-items-center">
-
-					<div class="connexion">
-						<form method="post" action="register.php">
-							<?php include('errors.php'); ?>
-
-							<div class="form-row">
-								<div class="form-group col-md-6">
-									<label for="Username">Username</label>
-									<input type="text" name="username" class="form-control" id="Username" value="<?php echo $username; ?>">
-								</div>
-								<div class="form-group col-md-6">
-									<label for="inputmail">Email</label>
-									<input type="email" class="form-control" id="mail" name="email" value="<?php echo $email; ?>" placeholder="Email"/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="inputPassword4">Adresse</label>
-								<input type="text" class="form-control" id="adress" name="adress" placeholder="Adresse"/>
-							</div>
-							<div class="form-row">
-								<div class="form-group col-md-6">
-									<label for="city">Ville</label>
-									<input type="text" name="city" class="form-control" id="city" placeholder="Ville"/>
-								</div>
-								<div class="form-group col-md-6">
-									<label for="cp">Code postal</label>
-									<input type="number" class="form-control" id="cp" name="cp" placeholder="Code postal"/>
-								</div>
-							</div>
-							<div class="form-group">
-								<label for="inputPassword4">Mot de passe</label>
-								<input type="password" class="form-control" id="inputPassword4" name="password_1" placeholder="Mot de passe"/>
-							</div>
-							<div class="form-group">
-								<label for="inputPassword4">Confirmation mot de passe</label>
-								<input type="password" class="form-control" id="inputPassword4" name="password_2" placeholder="Confirmation mot de passe"/>
-							</div>
-
-							<style>
-								italic {
-									font-style: italic;
-									float: left
-								}
-							</style>
-							<italic> En cliquant sur Register vous confirmez bien que vous êtes âgé de plus de 18 ans.</italic>
-							<div class="input-group">
-								<button type="submit" class="btn" name="reg_user">Register</button>
-							</div>
-
-
-							<p>
-								Already a member? <a href="login.php">Sign in</a>
-							</p>
-						</form>
-					</div>
-				</div>
-			</div>
-	</section>-->
 
 			<section class="register">
 				<div class="buttons_loginregister">
@@ -150,38 +72,7 @@
 				</div>
 			</section>
 
-			<!-- Footer -->
-			<!--<footer id="footer">
-		<div class="inner">
-			<div class="content">
-				<section>
-					<h3>Accumsan montes viverra</h3>
-					<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
-				</section>
-				<section>
-					<h4>Sem turpis amet semper</h4>
-					<ul class="alt">
-						<li><a href="#">Dolor pulvinar sed etiam.</a></li>
-						<li><a href="#">Etiam vel lorem sed amet.</a></li>
-						<li><a href="#">Felis enim feugiat viverra.</a></li>
-						<li><a href="#">Dolor pulvinar magna etiam.</a></li>
-					</ul>
-				</section>
-				<section>
-					<h4>Magna sed ipsum</h4>
-					<ul class="plain">
-						<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
-						<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
-						<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
-						<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
-					</ul>
-				</section>
-			</div>
-			<div class="copyright">
-				&copy; Untitled. Photos <a href="https://unsplash.co">Unsplash</a>, Video <a href="https://coverr.co">Coverr</a>.
-			</div>
-		</div>
-	</footer>-->
+
 		</div>
 	</div>
 </body>
