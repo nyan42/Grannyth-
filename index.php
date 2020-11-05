5ca91564b315dae2655187c20a868f24500e37e3
LydieV <49896819+LydieV@users.noreply.github.com>
2020-11-05 13:44:11 +0100

    Nouveau menu sur mainpage

--- index.php
+++ index.php
@@ -9,6 +9,14 @@ if (!isset($_SESSION['username'])) {
 	$username = $_SESSION['username'];
 }
 
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: registration/login.php");
+}
+
+require ("bdd/dbSet.php");
+
 ?>
 
 <!DOCTYPE HTML>
@@ -28,16 +36,22 @@ if (!isset($_SESSION['username'])) {
 
         <!-- Header -->
         <header id="header">
-            <a class="logo" href="index.php">SmartCityzen</a>
+            <a class="logo" href="index.php">
+                <img src="images/SmartCitizen_blanc.png" class="logo_img" />
+            </a>
             <nav>
-                <a href="#menu">Menu</a>
+                <a href="#menu"></a>
             </nav>
         </header>
 
         <!-- Nav -->
         <nav id="menu">
             <ul class="links">
-                <li><a href="index.php">Home</a></li>
+                <li><a href="PrendreUnePhoto.php">Prendre une photo </a></li>
+                <li><a href="post.php">Publier une proposition </a></li>
+                <li><a href="publicationGeneral.php">Voir les propositions </a></li>
+                <li><a href="registration/index.php"> Mon profil </a></li>
+                <li><a href="index.php?logout='1'">Déconnexion </a></li>
             </ul>
         </nav>
 
@@ -58,11 +72,11 @@ if (!isset($_SESSION['username'])) {
 
 
                 if ($isconnected == 1) { ?>
-                    <div class="ImageTrois">
+                    <!--<div class="ImageTrois">
                         <div class="textfacile">
                             <a href="registration/index.php"> Mon profil </a>
                         </div>
-                    </div>
+                    </div>-->
                 <?php } else { ?>
                     <header class="special">
                         <h2>Connectez-vous pour voter ou publier</h2>
@@ -78,12 +92,11 @@ if (!isset($_SESSION['username'])) {
                     </div>
                 <?php } ?>
 
-                </br></br>
 
                 <?php if ($isconnected == 1) { ?>
                     <div class="buttons_mainpage">
                         <a href="PrendreUnePhoto.php" class="button_CTA">Prendre une photo </a>
-                        <a href="post.php" class="button_CTA"> Poster une proposition </a>
+                        <a href="post.php" class="button_CTA"> Publier une proposition </a>
                         <a href="publicationGeneral.php" class="button_CTA"> Voir les propositions </a>
                     </div>
                     <!--	<div class="ImageTrois">
